6f6e07c9b331a5db8b11a19222db52bdbd5846f1
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-02-07 17:23:35 +0530

    some methods about numbers

--- AllAboutDataTypes/numbers.js
+++ AllAboutDataTypes/numbers.js
@@ -3,3 +3,14 @@ console.log(number);
 
 let score = new Number(100);
 console.log(score);
+console.log(score.toString());
+console.log(score.toFixed(2));
+
+let otherNumber = 12.89;
+
+console.log(otherNumber.toPrecision(3));
+
+/*
+    123.9 .toPrecision(3) => 124
+    123.89 .toPrecision(4) => 123.9
+*/
