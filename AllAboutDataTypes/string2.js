23bba393b1d4345a07bd1d1559158310292173c9
Shouvik1Sarkar <shouvik3501@gmail.com>
2025-02-13 17:06:12 +0530

    practice constrings

--- AllAboutDataTypes/string2.js
+++ AllAboutDataTypes/string2.js
@@ -36,3 +36,48 @@ console.log("Somraaj".length);
 
 // console.log(018 == "018");
 // console.log(017 == "017");
+
+const name1 = "Somraaj Majumdar Som";
+console.log(name1.slice(8, 16));
+const nameExtracted = name1.substr(8);
+console.log("...", nameExtracted);
+const nameUpperCase = name1.toUpperCase();
+console.log("++++++++", nameUpperCase);
+const nameLowererCase = name1.toLowerCase();
+console.log(nameLowererCase);
+console.log(name1.indexOf("a"));
+console.log(name1.endsWith("om"));
+
+const name3 = "Somraaj Majumdar Rohan";
+console.log("++++");
+
+console.log(name3.startsWith("Somraaj")); // Output: true
+console.log(name3.startsWith("So")); // Output: true
+console.log(name3.startsWith("Majumdar")); // Output: false
+
+const c = "   Name      ";
+console.log(c);
+console.log(c.trim());
+
+console.log(name3.includes("Somraaj"));
+console.log(name3.includes("So"));
+console.log(name3.includes("raa"));
+console.log(name3.includes("majumdar"));
+console.log(name3.includes("3"));
+console.log(name3.replaceAll("Majumdar", "lazy"));
+const paragraph = "I think Ruth's dog is cuter than your dog!";
+
+console.log(paragraph.replace("dog", "monkey"));
+
+console.log(name3.split(" "));
+let url = "https://x.com/home?lang=en";
+console.log(url.replaceAll("/", "-"));
+
+let firstName = "Somraaj";
+let serName = "Majumdar";
+let lastName = "Rohan";
+
+console.log(firstName.concat(" ", serName, " ", lastName));
+console.log("Hello! I am", firstName, serName);
+console.log("Hello! I am " + firstName + " " + serName);
+console.log(`Hello! I am ${firstName} ${serName}`);
